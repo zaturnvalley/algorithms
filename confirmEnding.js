3a94c5eab8493284436e96c817d4f94cdfb12ada
zaturnvalley <zacharycmason@gmail.com>
2016-10-14 14:44:31 -0700

    solved using substr

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,14 +1,12 @@
 //return true if target matches ending of str input, else otherwise
 
 
+
 function confirmEnding(str, target) {
-  var splitStr = str.split('');
-  if (target !== splitStr[splitStr.length -1]) {
+  var end = target.length;
+  var start = str.length - end;
+  if (target !== str.substr(start,end)) {
     return false;
   }
   return true;
 }
-
-confirmEnding("Bastian", "n");
-
-//this works for single words as str and single letter as targ, but not if target is a word, str is sent
