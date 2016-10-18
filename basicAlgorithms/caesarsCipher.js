704ea229d726d435d646095c3563874ba06fd098
zaturnvalley <zacharycmason@gmail.com>
2016-10-18 12:44:03 -0700

    capitals have different char codes. starts 65

--- basicAlgorithms/caesarsCipher.js
+++ basicAlgorithms/caesarsCipher.js
@@ -3,24 +3,23 @@ function rot13(str) { // LBH QVQ VG!
   var newStrArr = [];
   for (var i = 0; i < str.length; i++) {
     //sets blank to blank
-    if (str[i] === ''){
+    if (str[i] === ' '){
       newStrArr.push(str[i]);
     } 
     //gets char code
     var char = str[i].charCodeAt(0);
     
-    //if greater or eq to 110, wrap back to 97 (letter a)
-    if (char >= 110) {
-    newStrArr.push(String.fromCharCode(char - 13))
+    //if greater or eq to 77, wrap back to 65 (letter A)
+    if (char >= 77) {
+    newStrArr.push(String.fromCharCode(char - 13));
   }
     //else add 13 onto number
-    newStrArr.push(String.fromCharCode(char + 13))
+    else if (char < 77){
+      newStrArr.push(String.fromCharCode(char + 13))  
+    }
   }
   return newStrArr.join('');
 }
 
-// Change the inputs below to test
-rot13("LBH QVQ VG!");
-
-
-
+// // Change the inputs below to test
+rot13("SERR PBQR PNZ");
