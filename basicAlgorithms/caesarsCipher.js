85f8c60090e2973e02d070adcab38929aceeaffd
zaturnvalley <zacharycmason@gmail.com>
2016-10-18 12:37:19 -0700

    need middle alphabet to wrap

--- basicAlgorithms/caesarsCipher.js
+++ basicAlgorithms/caesarsCipher.js
@@ -1,12 +1,26 @@
+
 function rot13(str) { // LBH QVQ VG!
   var newStrArr = [];
   for (var i = 0; i < str.length; i++) {
+    //sets blank to blank
     if (str[i] === ''){
       newStrArr.push(str[i]);
-    } var char = str[i].charCodeAt(0);
+    } 
+    //gets char code
+    var char = str[i].charCodeAt(0);
+    
+    //if greater or eq to 110, wrap back to 97 (letter a)
+    if (char >= 110) {
+    newStrArr.push(String.fromCharCode(char - 13))
+  }
+    //else add 13 onto number
     newStrArr.push(String.fromCharCode(char + 13))
   }
   return newStrArr.join('');
 }
+
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+rot13("LBH QVQ VG!");
+
+
+
