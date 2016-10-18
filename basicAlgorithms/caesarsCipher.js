a44d41e648c34cf4994b74c3a86adebd5e73e8d9
zaturnvalley <zacharycmason@gmail.com>
2016-10-18 12:55:13 -0700

    solution for caesers cipher

--- basicAlgorithms/caesarsCipher.js
+++ basicAlgorithms/caesarsCipher.js
@@ -1,25 +1,23 @@
-
+//function returns ROT13 cipher of string
 function rot13(str) { // LBH QVQ VG!
   var newStrArr = [];
   for (var i = 0; i < str.length; i++) {
-    //sets blank to blank
-    if (str[i] === ' '){
-      newStrArr.push(str[i]);
-    } 
     //gets char code
     var char = str[i].charCodeAt(0);
     
     //if greater or eq to 77, wrap back to 65 (letter A)
-    if (char >= 77) {
+    if (char >= 78) {
     newStrArr.push(String.fromCharCode(char - 13));
   }
     //else add 13 onto number
-    else if (char < 77){
-      newStrArr.push(String.fromCharCode(char + 13))  
-    }
+    else if (char < 78 && char >= 65){
+      newStrArr.push(String.fromCharCode(char + 13)); 
+    } 
+    //sets all other characters to itself
+    else newStrArr.push(str[i]);
   }
   return newStrArr.join('');
 }
 
 // // Change the inputs below to test
-rot13("SERR PBQR PNZ");
+rot13("SERR CVMMN!");
