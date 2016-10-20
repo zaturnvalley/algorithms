2c01e8cbe8eb0ed26153ab6cf2f0a2c039c10f77
zaturnvalley <zacharycmason@gmail.com>
2016-10-20 16:43:57 -0700

    solution to roman numerals

--- intermediateAlgorithms/romanNumeralConverter.js
+++ intermediateAlgorithms/romanNumeralConverter.js
@@ -1,15 +1,26 @@
 function convertToRoman(num){
-  var range = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-  var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  var range = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000, 5000];
+  var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M', 'V'];
   
   var placeholder = num;
   var solution = '';
   
-  for (var i = 0; i < range.length; i++){
-    if (range[i] <= placeholder && placeholder < range[i+1]){
-      solution += roman[i];
-      placeholder -= range[i];
-    }
-  } return solution;
+  var j = 0;
+  
+  while (j < range.length){
+    for (var i = 0; i < range.length; i++){
+      if (range[i] <= placeholder && placeholder < range[i+1]){
+        solution += roman[i];
+        placeholder -= range[i];
+      }
+    } j++;
+  }
+
+
+  return solution;
 }
-convertToRoman(3);
+
+
+convertToRoman(3252);
+
+convertToRoman(19);
