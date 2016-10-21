e22d1fdfde6d23804299fbd33028c2ae5bbf1b5e
zaturnvalley <zacharycmason@gmail.com>
2016-10-20 20:41:38 -0700

    reworked / solved with filter

--- intermediateAlgorithms/whereforeArtThou.js
+++ intermediateAlgorithms/whereforeArtThou.js
@@ -1,17 +1,17 @@
+//if collection contains source, return that collection
 function whatIsInAName(collection, source) {
-  // What's in a name?
-  var arr = [];
-  // Only change code below this line
-  for(var i = 0; i < collection.length; i++){
-    for (var secondVal in source){
-      for (var val in collection[i]){
-      if(val === secondVal && !arr.includes(collection[i]) && collection[i][val] === source[secondVal]){
-      arr.push(collection[i]);  
-      }
-      }
-    }
-  }
-  return arr;
+
+  var sourceKeys = Object.keys(source);
+
+  return collection.filter(function(obj){
+    for (var i = 0; i < sourceKeys.length; i++){
+      for (var key in collection){
+        if (obj[sourceKeys[i]] !== source[sourceKeys[i]] || !obj.hasOwnProperty(sourceKeys[i])){
+          return false;
+        }
+      } 
+    } return true;
+  });
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
