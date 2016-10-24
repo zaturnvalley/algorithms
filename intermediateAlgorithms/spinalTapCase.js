d24ee5c52306ae89d77446b7a97de90211157bba
zaturnvalley <zacharycmason@gmail.com>
2016-10-24 16:29:10 -0700

    solution to spinal case involves backwards loops!

--- intermediateAlgorithms/spinalTapCase.js
+++ intermediateAlgorithms/spinalTapCase.js
@@ -1,22 +1,18 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 function spinalCase(str) {
   var splStr = str.split('');
-  var boolean = true;
-  if (boolean === true);
-  for (var i = 0; i < splStr.length; i++){
+
+  for (var i = splStr.length - 1; i > 0; i--){
     var charCode = splStr[i].toString().charCodeAt(0);
     if (splStr[i] === ' ' || splStr[i] === '_') {
       splStr[i] = '-';
     } 
-    else if(i > 0 && charCode >= 65 && charCode <= 90){
-      splStr.splice(i - 1, 0, '-');
+    else if(i > 0 && charCode >= 65 && charCode <= 90 && splStr[i -1] !== ' ' && splStr[i - 1] != '_'){
+      splStr.splice(i, 0, '-');
     }
-  }
+  } 
+
   return splStr.join('').toLowerCase();
 }
-console.log('p'.charCodeAt(0));
-spinalCase('This IsSpinal Tap');
 
-// var a = [2];
-// a.splice(1-1, 0, 'o');
-// console.log(a);
+spinalCase('This IsSpinal Tap');
