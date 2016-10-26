6b0bec996b30c87a6a02100fc7cac9e5ae0a5ad3
zaturnvalley <zacharycmason@gmail.com>
2016-10-26 16:16:23 -0700

    solution to smallestCommonMult

--- intermediateAlgorithms/smallestCommonMultiple.js
+++ intermediateAlgorithms/smallestCommonMultiple.js
@@ -1,20 +1,29 @@
+// noprotect
+
 //Find the smallest common multiple of the provided parameters that can be evenly divided by both, 
 //as well as by all sequential numbers in the range between these parameters.
-
 
 function smallestCommons(arr) {
   var range = [];
   for (var i = arr[0]; i <= arr[1]; i++){
     range.push(i);
   }
+  var multiple = range[range.length - 1];
 
+  var checker = false;
+  var checker2 = false;
+  
+  while(!checker){
+    while (!checker2){
+      for (var j = range[0]; j <= range[range.length - 1]; j++){
+      if (multiple % j !== 0){
+        checker2 = false;
+        break;
+      } checker2 = true;
+      } multiple++;
+    }
+
+    checker = true;
+  }
+  return multiple - 1;
 }
-
-
-smallestCommons([1,10]);
-
-//find primes
-//object contructor -- key value pairs of prime number maps to its max index 
-//set values in object to zero
-//check if factor is greater than zero, if so, update value
-//
