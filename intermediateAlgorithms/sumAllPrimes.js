643fe033a6242761a3a17a7258661a0bf1acb21f
zaturnvalley <zacharycmason@gmail.com>
2016-10-25 17:01:37 -0700

    solution to sumAllPrimes

--- intermediateAlgorithms/sumAllPrimes.js
+++ intermediateAlgorithms/sumAllPrimes.js
@@ -1,13 +1,29 @@
 //Sum all the prime numbers up to and including the provided number.
 
 //finds primes
+function sumPrimes(num) {
+  var sum = 0;
+ for (var i = 0; i <= num; i++){
+  if (prime(i)){
+    sum += i;
+  }
+ }
+ return sum;
+}
+
 function prime(num){
   var square = Math.floor(Math.sqrt(num));
-  
-  for (var i = 2; i <= square; i++){
-    if (num % i ===0){
-      return false;
-    } return true;
+  if (num === 1){
+    return false;
+  } else if (num === 2){
+    return true;
+  } else if (num === 3) {
+    return true;
   }
+  for (var i = 2; i <= square; i++){
+    if (num % i === 0){
+      return false;
+    } 
+  } return true;
 }
-prime(31);
+
