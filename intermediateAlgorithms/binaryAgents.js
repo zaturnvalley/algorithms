500a25034be57d6200c4f2107791c994c1e3ff1c
zaturnvalley <zacharycmason@gmail.com>
2016-10-28 12:18:43 -0700

    solution for binary agents

--- intermediateAlgorithms/binaryAgents.js
+++ intermediateAlgorithms/binaryAgents.js
@@ -4,10 +4,10 @@ function binaryAgent(str) {
   var converted = [];
   //put str into split array
   var arrStr = str.split(' ');
-  for (var i = 0; i < str.length; i++){
+  for (var i = 0; i < arrStr.length; i++){
     //loop over arr, push converted binary into converted
     converted.push(String.fromCharCode(parseInt(arrStr[i], 2)));
-  } return converted.join(' ');
+  } return converted.join('');
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
