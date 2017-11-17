51f994711b5c665ff207664e4f027db728c5c5a4
zaturnvalley <zacharycmason@gmail.com>
2017-11-16 20:08:09 -0800

    added 2 more varients of reverse string

--- basicAlgorithms/reverseString.js
+++ basicAlgorithms/reverseString.js
@@ -1,9 +1,25 @@
-//Reverse a string
-function reverseString(str) {
-  var splitStr = str.split('');
-  var revStr = splitStr.reverse();
-  var joinStr = revStr.join('');
-  return joinStr;
+//Reverse a string using reverse
+// function reverseString(str) {
+//   var splitStr = str.split('');
+//   var revStr = splitStr.reverse();
+//   var joinStr = revStr.join('');
+//   return joinStr;
+// }
+
+// using a for loop
+// function reverseString(str) {
+//   var splStr = str.split('');
+//   var solution = [];
+//   for (var i = splStr.length; i >= 0; i--){
+//     solution.push(splStr[i]);
+//   } return solution.join('');
+// }
+
+// using reduce
+function reverseString(str){
+  var splStr = str.split('');
+  return splStr.reduce(function(a,b){return [b].concat(a);}, []).join('');
 }
+module.exports = reverse;
 
 reverseString("hello");
