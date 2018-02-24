b406f21b6fd2c1dbc4a9b635fb61d85dcc10fdd9
zaturnvalley <zacharycmason@gmail.com>
2018-02-24 09:40:05 -0800

    added solution 2 to chunk

--- otherAlgorithms/chunk.js
+++ otherAlgorithms/chunk.js
@@ -25,4 +25,18 @@ function chunk(array, size) {
   } return newArr;
 }
 
-module.exports = chunk;
+// solution #2
+
+function chunkTwo(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element])
+    } else {
+      last.push(element);
+    }
+  } return chunked;
+}
